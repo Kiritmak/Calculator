30e9772be44251634f1c82064a0c7ca2cf3d0438
Kiritmak <iran07ma@gmail.com>
2025-04-13 20:38:52 -0400

    Creada la base de la calculadora, con algunas excepciones para comprobar el input: -Implementadas las operaciones basicas -Implementados unos mensajes basicos -Posibilidad de salir de la aplicacion con la opcion 0 -Creadas las excpeciones para la divison por 0 y un rango no aceptable de la variable opcion

--- Program.cs
+++ Program.cs
@@ -7,6 +7,36 @@ namespace Calculadora
     static void Main(string[] args)
     {
       Console.WriteLine("Bienvenido a Calculadora!");
+      while(true)
+      {
+        Console.WriteLine("Selecciona una opcion!");
+        Console.WriteLine("1 : sumar");
+        Console.WriteLine("2 : restar");
+        Console.WriteLine("3 : multiplicar");
+        Console.WriteLine("4 : dividir");
+        Console.WriteLine("0 : cerrar la calculadora");
+        int opcion = int.Parse(Console.ReadLine());
+        int a, b;
+        if(opcion == 0)
+        {
+          Console.WriteLine("Gracias por usar la calculadora!");
+          break;
+        }
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        switch (opcion)
+        {
+          case 1 : Console.WriteLine("El resultado es {0}", a+b); break;
+          case 2 : Console.WriteLine("El resultado es {0}", a-b); break;
+          case 3 : Console.WriteLine("El resultado es {0}", a*b); break;
+          case 4 : 
+          {
+            if(b==0) throw new DivideByZeroException();
+            Console.WriteLine("El resultado es {0}", a/b); break;
+          }
+          default : throw new ArgumentException("Input Incorrecta, opcion"); 
+        }
+      }
     }
   }
 }
