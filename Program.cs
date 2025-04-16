d000915e8a7ad63c32b1d3a3951bea97ae2e2f66
Kiritmak <iran07ma@gmail.com>
2025-04-15 21:56:27 -0400

    Eliminada la "interfaz" de consola Ahora se comporta como una verdadera calculadora El programa cierra con "close"

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 
 namespace Calculadora
 {
@@ -10,36 +11,21 @@ namespace Calculadora
       int opcion;
       do
       {
-        decimal a, b;
-        Console.WriteLine("Selecciona una opcion");
-        Console.WriteLine("1 : sumar");
-        Console.WriteLine("2 : restar");
-        Console.WriteLine("3 : multiplicar");
-        Console.WriteLine("4 : dividir");
-        Console.WriteLine("0 : cerrar la calculadora");
-        if(!int.TryParse(Console.ReadLine(), out opcion) || opcion<0 || 4 < opcion) 
+        DataTable dt = new DataTable();
+        Object result;
+
+        string? calculation = Console.ReadLine();
+        if(calculation.ToLower() == "close") break;
+        try 
         {
-          Console.WriteLine("Input Incorrecta");
+          result = dt.Compute(calculation, "");
+        }
+        catch (Exception e) 
+        {
+          Console.WriteLine("Entrada Incorrecta : {0}", e.Message);
           continue;
         }
-        if(opcion==0) break;
-        if(!decimal.TryParse(Console.ReadLine(), out a) || !decimal.TryParse(Console.ReadLine(), out b))
-        {
-          Console.WriteLine("Input Incorrecta");
-          continue;
-        }
-        if(opcion==1) Console.WriteLine("El resultado es: {0}", a+b); 
-        else if(opcion==2) Console.WriteLine("El resultado es: {0}", a-b);
-        else if(opcion==3) Console.WriteLine("El resultado es: {0}", a*b);
-        else if(opcion==4)
-        {
-          if(b==0) 
-          {
-            Console.WriteLine("No se puede dividir por cero");
-            continue;
-          }
-          Console.WriteLine("El resultado es: {0}", a/b);
-        }
+        Console.WriteLine(result);
       } while(true);
       Console.WriteLine("Gracias por usar la calculadora!!!!!!");
     }
--- obj/Debug/net9.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c38e941dae7a6b649a96376890d7677a5f5e58d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2b217869b1afc0d64c508e3cd818949baa4934b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
